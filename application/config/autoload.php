0f5d5b5e59adf9e106c660a2f0c3a4510ad51fb0
Paul Ferra <paul@etags.com>
2019-12-09 12:34:26 -0500

    IG API

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
   |
   |	$autoload['libraries'] = array('user_agent' => 'ua');
  */
-$autoload['libraries'] = array('database', 'session', 'instagram_api', 'encrypt');
+$autoload['libraries'] = array('database', 'session', 'encrypt');
 
 /*
   | -------------------------------------------------------------------

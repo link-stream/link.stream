a9b0352bdd5407ea9b693b35ffcd94e0b3a06e6a
Paul Ferra <paul@etags.com>
2019-12-09 12:48:31 -0500

    IG Config

--- application/config/instagram_api.php
+++ application/config/instagram_api.php
@@ -1,21 +1,42 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Instagram
-|--------------------------------------------------------------------------
-|
-| Instagram client details
-|
-*/
+  |--------------------------------------------------------------------------
+  | Instagram
+  |--------------------------------------------------------------------------
+  |
+  | Instagram client details
+  |
+ */
 
-$config['instagram_client_name']	= 'Streamy.link';
-$config['instagram_client_id']		= '581144849352056';
-$config['instagram_client_secret']	= '79b850e476af6aa7d6ec00ecc880f464';
-$config['instagram_callback_url']	= 'https://localhost/streamy.link/app/instagram_callback';//e.g. http://yourwebsite.com/authorize/get_code/
-$config['instagram_website']		= 'https://localhost/streamy.link';//e.g. http://yourwebsite.com/
-$config['instagram_description']	= '';
-	
+
+$url = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : 'www.streamy.link';
+$chk = explode('.', $url);
+if ($chk[0] == "localhost") {
+    $config['instagram_client_name'] = 'Streamy.link';
+    $config['instagram_client_id'] = '581144849352056';
+    $config['instagram_client_secret'] = '79b850e476af6aa7d6ec00ecc880f464';
+    $config['instagram_callback_url'] = 'https://localhost/streamy.link/app/instagram_callback'; //e.g. http://yourwebsite.com/authorize/get_code/
+    $config['instagram_website'] = 'https://localhost/streamy.link'; //e.g. http://yourwebsite.com/
+    $config['instagram_description'] = '';
+    //$db['default']['db_debug'] = TRUE;
+} elseif ($chk[0] == "streamylinkdevenv") {
+    $config['instagram_client_name'] = 'Streamy.link';
+    $config['instagram_client_id'] = '581144849352056';
+    $config['instagram_client_secret'] = '79b850e476af6aa7d6ec00ecc880f464';
+    $config['instagram_callback_url'] = 'https://streamylinkdevenv.us-east-2.elasticbeanstalk.com/app/instagram_callback'; //e.g. http://yourwebsite.com/authorize/get_code/
+    $config['instagram_website'] = 'https://streamylinkdevenv.us-east-2.elasticbeanstalk.com/'; //e.g. http://yourwebsite.com/
+    $config['instagram_description'] = '';
+} else {
+    $config['instagram_client_name'] = 'Streamy.link';
+    $config['instagram_client_id'] = '581144849352056';
+    $config['instagram_client_secret'] = '79b850e476af6aa7d6ec00ecc880f464';
+    $config['instagram_callback_url'] = ''; //e.g. http://yourwebsite.com/authorize/get_code/
+    $config['instagram_website'] = ''; //e.g. http://yourwebsite.com/
+    $config['instagram_description'] = '';
+}
+
+
 /**
  * Instagram provides the following scope permissions which can be combined as likes+comments
  * 
@@ -30,4 +51,4 @@ $config['instagram_scope'] = '';
 // There was issues with some servers not being able to retrieve the data through https
 // If you have this problem set the following to FALSE 
 
-$config['instagram_ssl_verify']		= TRUE;
+$config['instagram_ssl_verify'] = TRUE;

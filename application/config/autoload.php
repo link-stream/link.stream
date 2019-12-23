53e67233f4d1b254f97a6bb54f5b831ce6462764
Paul Ferra <paul@etags.com>
2019-12-22 19:31:32 -0500

    Aut

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
   |
   |	$autoload['libraries'] = array('user_agent' => 'ua');
  */
-$autoload['libraries'] = array('database', 'session', 'Encryption', 'General_library','Encrypt');
+$autoload['libraries'] = array('database', 'session', 'Encryption', 'General_library');
 
 /*
   | -------------------------------------------------------------------

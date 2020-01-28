fed4fbad1e181c56498a3d619ce2a575bea9f9a0
anneris.wong <anneris.wong@get.tur.cu>
2020-01-28 10:15:19 -0800

    Submenu v1

--- application/views/app/layouts/layout.php
+++ application/views/app/layouts/layout.php
@@ -16,10 +16,10 @@
                 <q-layout view="hHh lpR fFf">
                     <q-header reveal class="bg-black text-white" style="height: 80px">
                         <q-toolbar>
-                            <q-toolbar-title class="row items-center">
+                            <q-toolbar-title class="row items-center content-center">
                                 <img src="assets/images/logo/streamy_icon_RGB.png" alt="streamy" class="q-mt-sm q-pa-none streamy_logo" />
-                                <img :src="menuState" @click="left = !left" alt="menu" class="q-mt-sm q-ml-sm cursor-pointer menu_icon" />
-                            </q-toolbar-title>
+                                <img :src="menuState" @click="left = !left" alt="menu" class="q-mt-sm q-ml-sm cursor-pointer menu_icon" />      
+                            </q-toolbar-title>                                                  
                             <div class="q-gutter-sm row items-center q-mr-sm">
                                 <q-btn round dense flat color="grey-8" icon="img:assets/images/icons/icon-alerts.svg" class="q-mr-none">
                                     <q-badge color="red-12" text-color="white" floating>2</q-badge>
@@ -55,8 +55,8 @@
                                 </q-btn>                                    
                             </div>
                         </q-toolbar>
-                    </q-header>
-                    <q-drawer show-if-above v-model="left" side="left" :width="128" :content-style="{backgroundColor: '#333333'}">
+                    </q-header>                       
+                    <!--q-drawer-- show-if-above v-model="left" side="left" :width="128" :content-style="{backgroundColor: '#333333'}">
                         <q-scroll-area class="fit">
                             <q-list v-for="(menuItem, index) in menuList" :key="index" class="text-grey-8" separator dark>
                                 <q-item clickable style="height: 85px" class="column items-center content-center q-pa-sm">
@@ -66,16 +66,51 @@
                                     <q-item-section class="q-mx-none q-mb-sm q-my-none">
                                         <q-item-label  class="font-menu">{{ menuItem.label }}</q-item-label>
                                     </q-item-section> 
-                                    <q-slide-transition>                                                                                                                              
+                                    <q-slide-transition>                                                                                              
                                 </q-item>
                                 <q-separator dark/>
                             </q-list>                            
-                        </q-scroll-area>                        
-                    </q-drawer>
-                    
-                    <q-page-container>
-                        <?php $this->load->view('app/partials/' . $body_content); ?>
-                    </q-page-container>
+                        </q-scroll-area>                                                
+                    </!--q-drawer--> 
+                    <q-drawer show-if-above v-model="left" side="left" :width="dynamicWidth" :content-style="{backgroundColor: '#333333'}">                        
+                        <q-scroll-area class="fit">
+                            <div class="row">
+                                <div class="col">
+                                    <q-card class="my-card" style="width:128px; height: 93vh; background-color: #333333; color: white" square flat>                        
+                                        <q-list v-for="(menuItem, index) in menuList" :key="index" class="text-grey-8">
+                                            <q-item clickable style="height: 85px" class="column items-center content-center q-pa-sm" :active="selectedMenu === 'index'" active-class="my-menu-link">
+                                                <q-item-section class="q-ml-none q-mt-md">                                       
+                                                    <q-icon color="white" size="20px" :name="menuItem.icon" />
+                                                </q-item-section>
+                                                <q-item-section class="q-mx-none q-mb-sm q-my-none">
+                                                    <q-item-label  class="font-menu">{{ menuItem.label }}</q-item-label>
+                                                </q-item-section> 
+                                                <q-slide-transition>                                                                                                                                      
+                                            </q-item>
+                                            <q-separator dark/>   
+                                        </q-list>
+                                    </q-card>
+                                </div>
+                                <div class="col" style="width:160px" v-show="submenu">
+                                    <q-card class="my-card" style="width:160px; height: 93vh; background-color: rgba(0,0,0,0.5); color: white" square flat>                        
+                                        <q-list v-for="(menuItem, index) in menuList" :key="index" class="text-grey-8">
+                                            <q-item clickable style="height: 85px" class="column items-center content-center q-pa-sm">                                                
+                                                <q-item-section class="q-mx-none q-mb-sm q-my-none">
+                                                    <q-item-label  class="font-menu">{{ menuItem.label }}</q-item-label>
+                                                </q-item-section> 
+                                                <q-slide-transition>                                                                                              
+                                            </q-item> 
+                                        </q-list>
+                                    </q-card>    
+                                </div>                
+                            </div>                            
+                        </q-scroll-area>  
+                    </q-drawer>                      
+                    <v-div>
+                        <q-page-container>
+                            <?php $this->load->view('app/partials/' . $body_content); ?>
+                        </q-page-container>
+                    </v-div> 
                 </q-layout>
             </template>
 
@@ -93,17 +128,74 @@
                             version: Quasar.version,
                             drawerState: true,
                             left: false,
+                            dynamicWidth: 128,
                             menuState: '',
+                            submenu: null,
+                            selectedMenu: '0',
                             menuList: [
-                                { icon: 'img:assets/images/icons/tachometer-alt.svg', label: 'Dashboard' },
-                                { icon: 'img:assets/images/icons/icon-user-circle.svg', label: 'My Profile' },
-                                { icon: 'img:assets/images/icons/icon-tracks.svg', label: 'My Tracks' },
-                                { icon: 'img:assets/images/icons/icon-user-circle.svg', label: 'My Content' },
-                                { icon: 'img:assets/images/icons/icon-link.svg', label: 'My Links' },
-                                { icon: 'img:assets/images/icons/chart-bar.svg', label: 'Analytics' },
-                                { icon: 'img:assets/images/icons/icon-reports.svg', label: 'Reports' },
-                                { icon: 'img:assets/images/icons/icon-upgrade.svg', label: 'Upgrade' },
-                                { icon: 'img:assets/images/icons/icon-settings.svg', label: 'Account Settings' }
+                                { 
+                                    icon: 'img:assets/images/icons/tachometer-alt.svg', 
+                                    label: 'Dashboard',
+                                    linkTo: '#', 
+                                    submenu: [] 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-user-circle.svg', 
+                                    label: 'My Profile', 
+                                    linkTo: '#',
+                                    submenu: [
+                                        { linkTo: '#', label: 'Edit Profile' },
+                                        { linkTo: '#', label: 'View Profile' }
+                                    ] 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-tracks.svg', 
+                                    label: 'My Tracks',
+                                    linkTo: '#',
+                                    submenu: [
+                                        { linkTo: '#', label: 'Add New Tracks' },
+                                        { linkTo: '#', label: 'Manage Tracks' },
+                                        { linkTo: '#', label: 'Manage Reposts' },
+                                    ] 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-user-circle.svg', 
+                                    label: 'My Content',
+                                    linkTo: '#',
+                                    submenu: [
+                                        { linkTo: '#', label: 'Add Content' },
+                                        { linkTo: '#', label: 'Manage Content' }
+                                    ]  
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-link.svg', 
+                                    label: 'My Links',
+                                    linkTo: '#',
+                                    submenu: [
+                                        { linkTo: '#', label: 'Add Links' },
+                                        { linkTo: '#', label: 'Manage Links' }
+                                    ]  
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/chart-bar.svg', 
+                                    label: 'Analytics',
+                                    linkTo: '#', 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-reports.svg', 
+                                    label: 'Reports',
+                                    linkTo: '#', 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-upgrade.svg', 
+                                    label: 'Upgrade',
+                                    linkTo: '#', 
+                                },
+                                { 
+                                    icon: 'img:assets/images/icons/icon-settings.svg', 
+                                    label: 'Account Settings',
+                                    linkTo: '#', 
+                                }
                             ],
                             notificationsMenu: [
                                 { icon: 'assets/images/testimonials/1.png', notification: 'theredfantastic started following you 1h' },
@@ -117,12 +209,14 @@
                         }
                     },
                     mounted() {
-                        console.log(this.size)
-                        console.log(window.screen.width)
+                        this.submenu = true;
                     },
                     watch: {
                         left () {
                             (this.left === false) ? this.menuState = 'assets/images/icons/bars.svg' : this.menuState = 'assets/images/icons/icon-close-sharp.svg';
+                        },
+                        submenu () {
+                            this.submenu ? this.dynamicWidth = 288 : 128;
                         }
                     },
                     methods: {

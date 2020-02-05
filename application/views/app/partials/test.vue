6a2a8aa5f5a3748317aa87de2c9e34af794028fb
lisbet.hernandez <lisbet.hernandez@get.tur.cu>
2020-02-05 11:32:18 -0500

    test routing menu

---
+++ application/views/app/partials/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>Test</>
+</template>
+<script>    
+
+
+export default {
+  name: "test",
+  data () {
+    return {
+      
+
+    }
+  },
+
+
+  mounted () {
+  },
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script> 
+<style>
+</style>

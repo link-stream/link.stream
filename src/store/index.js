513553070f2e855ae821740f480a01a2d06df152
Victor Lantigua <victor@etags.com>
2020-03-29 17:58:18 -0400

    Remove logger plugin (not needed)

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createLogger from '~/plugins/logger'
 
 Vue.use(Vuex)
 
-const debug = process.env.NODE_ENV !== 'production'
-
-// Load store modules dynamically.
+// Load store modules dynamically
 const requireContext = require.context('./modules', false, /.*\.js$/)
 
 const modules = requireContext
     .keys()
     .map(file => [file.replace(/(^.\/)|(\.js$)/g, ''), requireContext(file)])
     .reduce((modules, [name, module]) => {
-        modules[name] = { ...module }
+        modules[name] = {
+            ...module,
+        }
         return modules
     }, {})
 
-const plugins = []
-
 export default new Vuex.Store({
     modules: modules,
-    strict: debug,
-    plugins: debug ? plugins.concat([createLogger()]) : plugins,
+    strict: process.env.NODE_ENV !== 'production',
 })

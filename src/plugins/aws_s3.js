f26c5ddbd94020612e368b16f7dab3b9afe0c67b
Victor Lantigua <victor@etags.com>
2020-04-01 17:37:41 -0400

    Remove AWS credentials envs

--- src/plugins/aws_s3.js
+++ src/plugins/aws_s3.js
@@ -1,11 +1,11 @@
 import S3 from 'aws-s3'
 
 const config = {
-    accessKeyId: process.env.VUE_APP_GOOGLE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.VUE_APP_GOOGLE_AWS_ACCESS_SECRET_KEY,
-    region: process.env.VUE_APP_GOOGLE_AWS_REGION,
-    bucketName: process.env.VUE_APP_GOOGLE_AWS_BUCKET,
-    dirName: process.env.VUE_APP_GOOGLE_AWS_DIR,
+    accessKeyId: null,
+    secretAccessKey: null,
+    region: null,
+    bucketName: null,
+    dirName: null,
 }
 
 const S3Client = new S3(config)

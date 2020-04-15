611a6a5983af34245352a81c10901c8d5016d513
Victor Lantigua <victor@etags.com>
2020-04-15 07:58:56 -0400

    Prettier formatting

--- src/pages/UserAccount/Links/Links.vue
+++ src/pages/UserAccount/Links/Links.vue
@@ -2,20 +2,28 @@
     <div class="page page-ua-links">
         <header class="page__header">
             <h1 class="page__title">Your links</h1>
-            <h4 class="page__subtitle">Add, remove, edit &amp; order links anyway you'd like.</h4>
+            <h4 class="page__subtitle">
+                Add, remove, edit &amp; order links anyway you'd like.
+            </h4>
         </header>
         <nav class="page__nav">
             <div class="page__nav__left">
                 <span class="preview-url">
-                    <span class="preview-url__light">link.stream/</span>
-                    {{ user.user_name }}/links
+                    <span class="preview-url__light">link.stream/</span
+                    >{{ user.user_name }}/links
                 </span>
                 <preview-pill-button
-                    :to="{ name: 'userLinks', params: { username: user.user_name }}"
-                >Preview</preview-pill-button>
+                    :to="{
+                        name: 'userLinks',
+                        params: { username: user.user_name },
+                    }"
+                    >Preview</preview-pill-button
+                >
             </div>
             <div class="page__nav__right">
-                <basic-button :to="{ name: 'userAccountLinksAdd' }">Add New Link</basic-button>
+                <basic-button :to="{ name: 'userAccountLinksAdd' }"
+                    >Add New Link</basic-button
+                >
             </div>
         </nav>
         <main class="page__body"></main>

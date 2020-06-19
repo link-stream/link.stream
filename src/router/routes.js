f1f50abb8dc441ab01094b889c09b8f9c816bb47
Victor Lantigua <victor@etags.com>
2020-06-19 08:23:21 -0400

    Rename layout files to lowercase per component docs

--- src/router/routes.js
+++ src/router/routes.js
@@ -22,7 +22,7 @@ import {
     AccountTracksLicenses,
     AccountTracksLicenseEdit,
     AccountSoundKits,
-    AccountSoundKitAdd
+    AccountSoundKitAdd,
 } from '~/pages/Account'
 
 const routes = [
@@ -33,13 +33,13 @@ const routes = [
         path: '/',
         name: 'home',
         component: ComingSoon,
-        meta: { layout: 'Landing' },
+        meta: { layout: 'landing' },
     },
     {
         path: '/legal',
         name: 'legal',
         component: Legal,
-        meta: { layout: 'Landing' },
+        meta: { layout: 'landing' },
     },
 
     /**
@@ -49,7 +49,7 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login,
-        meta: { requiresGuest: true, layout: 'Auth' },
+        meta: { requiresGuest: true, layout: 'auth' },
     },
     {
         path: '/logout',
@@ -61,31 +61,31 @@ const routes = [
         path: '/signup',
         name: 'signup',
         component: Signup,
-        meta: { requiresGuest: true, layout: 'Auth' },
+        meta: { requiresGuest: true, layout: 'auth' },
     },
     {
         path: '/register-confirm',
         name: 'signupConfirm',
         component: SignupConfirm,
-        meta: { requiresGuest: true, layout: 'Auth' },
+        meta: { requiresGuest: true, layout: 'auth' },
     },
     {
         path: '/email-confirm/:param1/:param2',
         name: 'emailConfirm',
         component: EmailConfirm,
-        meta: { requiresGuest: true, layout: 'Default' },
+        meta: { requiresGuest: true, layout: 'default' },
     },
     {
         path: '/forgot',
         name: 'passwordForgot',
         component: PasswordForgot,
-        meta: { requiresGuest: true, layout: 'Auth' },
+        meta: { requiresGuest: true, layout: 'auth' },
     },
     {
         path: '/reset-password/:param1/:param2',
         name: 'passwordReset',
         component: PasswordReset,
-        meta: { requiresGuest: true, layout: 'Auth' },
+        meta: { requiresGuest: true, layout: 'auth' },
     },
 
     /**
@@ -96,79 +96,79 @@ const routes = [
         name: 'accountDashboard',
         alias: '/app/dashboard',
         component: AccountDashboard,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/profile/edit',
         name: 'accountProfileEdit',
         component: AccountProfileEdit,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/videos/manage',
         name: 'accountVideos',
         component: AccountVideos,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/videos/add',
         name: 'accountVideoAdd',
         component: AccountVideoAdd,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/links/manage',
         name: 'accountLinks',
         component: AccountLinks,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/links/add',
         name: 'accountLinkAdd',
         component: AccountLinkAdd,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/beats/manage',
         name: 'accountBeats',
         component: AccountBeats,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/beats/add',
         name: 'accountBeatAdd',
         component: AccountBeatAdd,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/beats/:id/edit',
         name: 'accountBeatEdit',
         component: AccountBeatEdit,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/licenses/manage',
         name: 'accountTracksLicenses',
         component: AccountTracksLicenses,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/licenses/:id/edit',
         name: 'accountTracksLicenseEdit',
         component: AccountTracksLicenseEdit,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/kits/manage',
         name: 'accountSoundKits',
         component: AccountSoundKits,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
     {
         path: '/app/kits/add',
         name: 'accountSoundKitAdd',
         component: AccountSoundKitAdd,
-        meta: { requiresAuth: true, layout: 'Account' },
+        meta: { requiresAuth: true, layout: 'account' },
     },
 
     /**
@@ -179,7 +179,7 @@ const routes = [
         alias: '*',
         name: '404',
         component: NotFound,
-        meta: { layout: 'Error' },
+        meta: { layout: 'error' },
     },
 ]
 

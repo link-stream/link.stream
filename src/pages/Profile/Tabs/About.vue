ceca6b0efa15ab055ccbef79c75f85f89d3424f0
listar2016 <listar0117@gmail.com>
2020-08-10 12:17:47 -0400

    add about page

--- src/pages/Profile/Tabs/About.vue
+++ src/pages/Profile/Tabs/About.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>About</h1>
+    <div class="about-page">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis, sem sit amet ultricies volutpat, turpis neque dapibus quam, a lobortis mauris nisi nec mauris. Suspendisse molestie lacus et congue accumsan. Maecenas malesuada lectus elit, nec cursus risus volutpat et. Integer iaculis metus et posuere iaculis. In posuere magna libero. Suspendisse potenti. Proin nec commodo dui.</p>
+        <p>Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec euismod purus. Integer eget erat malesuada, mollis lorem eu, mollis nibh. Donec sit amet elit eu tellus iaculis ornare sodales eget felis. Etiam sit amet mauris luctus, consectetur tortor nec, consectetur justo. Etiam accumsan erat in varius tincidunt.</p>
+    </div>
 </template>
 <script>
 export default {

d39b9f81e1767f4287f3c7a2c8a9d3a589af13ec
ziggito <ziggito2020@gmail.com>
2020-10-19 13:27:07 -0400

    Upload folder Sales

---
+++ src/pages/Account/Sales/Sales.vue
@@ -0,0 +1,16 @@
+<!--template>
+<div>
+    <b-row>
+        <b-col>
+            <span>Sales</span>
+        </b-col>
+        <b-col>
+            <span>Paginado</span>
+        </b-col>
+    </b-row>
+    <b-row>
+        
+    </b-row>
+
+</div>
+</template-->

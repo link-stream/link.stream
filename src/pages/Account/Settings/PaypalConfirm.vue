4271b5774204a04839ec547cb0e7adac2436364e
listar2016 <listar0117@gmail.com>
2020-10-29 15:15:54 -0400

    test access token

--- src/pages/Account/Settings/PaypalConfirm.vue
+++ src/pages/Account/Settings/PaypalConfirm.vue
@@ -5,12 +5,39 @@
     </div>
 </template>
 <script>
+import axios from '~/plugins/axios'
+import qs from 'qs'
 export default {
     name: 'PaypalConfirm',
     computed: {
         authCode() {
             return this.$route.query.code
         }
+    },
+    mounted() {
+        const auth_code = this.$route.query.code
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        const auth = {
+            username: process.env.VUE_APP_PAYPAL_CLIENT_ID,
+            password: process.env.VUE_APP_PAYPAL_SECRET,
+        }
+        const endpoint = 'https://api.sandbox.paypal.com/v1/oauth2/token'
+        const params = {
+            grant_type: 'authorization_code',
+            code: auth_code
+        }
+
+        let payload
+        const response = await axios({
+            method: 'POST',
+            url: endpoint,
+            data: qs.stringify(params),
+            headers,
+            auth,
+        })
+        console.log(response)
     }
 }
 </script>

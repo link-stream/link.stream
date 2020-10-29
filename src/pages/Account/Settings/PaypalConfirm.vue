d047218aca81adea0c4fa14f3887f3e643d46b08
listar2016 <listar0117@gmail.com>
2020-10-29 15:49:40 -0400

    get paypal user information

--- src/pages/Account/Settings/PaypalConfirm.vue
+++ src/pages/Account/Settings/PaypalConfirm.vue
@@ -1,7 +1,33 @@
 <template>
-    <div>
-        <h1>Paypal Authorization Code</h1>
-        <p>{{ authCode }}</p>
+    <div class="page page-account-settings">
+        <h1 class="page-title">Paypal information</h1>
+        <LoadingSpinner
+            class="m-5"
+            animation="bounce"
+            v-if="loading"
+        />
+        <div v-else class="page-body">
+            <p>
+                <span class="font-weight-bold">Auth Code:</span>
+                {{ this.auth_code }}
+            </p>
+            <p>
+                <span class="font-weight-bold">Access Token:</span>
+                {{ this.access_token }}
+            </p>
+            <p>
+                <span class="font-weight-bold">Refresh Token:</span>
+                {{ this.refresh_token }}
+            </p>
+            <p>
+                <span class="font-weight-bold">User Id:</span>
+                {{ this.user_id }}
+            </p>
+            <p>
+                <span class="font-weight-bold">Email:</span>
+                {{ this.email }}
+            </p>
+        </div>
     </div>
 </template>
 <script>
@@ -9,35 +35,69 @@ import axios from '~/plugins/axios'
 import qs from 'qs'
 export default {
     name: 'PaypalConfirm',
-    computed: {
-        authCode() {
-            return this.$route.query.code
+    data: () =>({
+        auth_code: '',
+        access_token: '',
+        refresh_token: '',
+        user_id: '',
+        email: '',
+    }),
+    async created() {
+        this.auth_code = this.$route.query.code
+        if (this.auth_code) {
+            await this.getUserInfo()
+        }
+        if (this.access_token) {
+            await this.getUserInfo()
         }
     },
-    async mounted() {
-        const auth_code = this.$route.query.code
-        const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-        const auth = {
-            username: process.env.VUE_APP_PAYPAL_CLIENT_ID,
-            password: process.env.VUE_APP_PAYPAL_SECRET,
-        }
-        const endpoint = 'https://api.sandbox.paypal.com/v1/oauth2/token'
-        const params = {
-            grant_type: 'authorization_code',
-            code: auth_code
-        }
+    methods: {
+        async getAccessToken() {
+            const headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+            const auth = {
+                username: process.env.VUE_APP_PAYPAL_CLIENT_ID,
+                password: process.env.VUE_APP_PAYPAL_SECRET,
+            }
+            const endpoint = 'https://api.sandbox.paypal.com/v1/oauth2/token'
+            const params = {
+                grant_type: 'authorization_code',
+                code: this.auth_code
+            }
 
-        let payload
-        const response = await axios({
-            method: 'POST',
-            url: endpoint,
-            data: qs.stringify(params),
-            headers,
-            auth,
-        })
-        console.log(response)
+            const { status, data } = await axios({
+                method: 'POST',
+                url: endpoint,
+                data: qs.stringify(params),
+                headers,
+                auth,
+            })
+            if (status === 200) {
+                this.access_token = data.access_token
+                this.refresh_token = data.refresh_token
+            } else {
+                this.$toast.error(response.data.error_description)
+            }
+        },
+        async getUserInfo() {
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.access_token}`
+            }
+            const endpoint = 'https://api.sandbox.paypal.com/v1/identity/oauth2/userinfo?schema=paypalv1.1'
+            const { status, data } = await axios({
+                method: 'GET',
+                url: endpoint,
+                headers,
+            })
+            if (status === 200) {
+                this.user_id = data.user_id
+                this.email = data.emails.value
+            } else {
+                this.$toast.error(response.data.error_description)
+            }
+        }
     }
 }
 </script>
